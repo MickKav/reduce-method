fe3aa46d78a212ff2b31a83eb1b17fa6037460b0
MickKav <122673686+MickKav@users.noreply.github.com>
2023-08-10 19:15:10 +0100

    Initial commit

---
+++ reduce.js
@@ -0,0 +1,37 @@
+  
+/**
+ * To run this file in Gitpod, use the 
+ * command node reduce.js in the terminal
+ */
+
+
+// Summing an array of numbers:
+
+
+const teamMembers = [
+  {
+    name: 'Andrew',
+    profession: 'Developer',
+    yrsExperience: 5
+  },
+  {
+    name: 'Ariel',
+    profession: 'Developer',
+    yrsExperience: 7
+  },
+  {
+    name: 'Michael',
+    profession: 'Designer',
+    yrsExperience: 1
+  },
+  {
+    name: 'Kelly',
+    profession: 'Designer',
+    yrsExperience: 3
+  }
+];
+
+// Totaling a specific object property
+
+
+// Grouping by a property, and totaling it too
