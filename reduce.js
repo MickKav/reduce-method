da5ca92c0317df836e32dedb95911e6d5ef61652
MickKav <>
2023-08-10 18:36:19 +0000

    Initial commit

--- reduce.js
+++ reduce.js
@@ -6,6 +6,19 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+
+/*let sum = nums.reduce((acc, curr) => {
+    console.log(
+        "Accumulator:", acc,
+        "Current Value:", curr,
+        "Total:", acc + curr
+    );
+    return acc + curr;
+});
+*/
+console.log(sum);
 
 
 const teamMembers = [
